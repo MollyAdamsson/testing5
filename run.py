b1c11c1bd831be3e67aef64878804a0aa4a94148
Molly Adamsson <adamssonmolly@gmail.com>
2023-01-26 14:12:26 +0000

    Add content

--- run.py
+++ run.py
@@ -8,28 +8,40 @@ player_inventory = ["A healing potion"]
 
 def start_game():
     # Introduction and initial setup
-    print("Welcome to the this adventure game! You are about to embark on an epic journey filled with challenges and mysterious obstacles. Your goal is to complete each task so that you can reach your village on the other side of the woods. Are you ready for the adventure of a lifetime? ")
+    print("Welcome to the this adventure game!")
+    print("You are about to embark on an epic journey filled with challenges and mysterious obstacles.")
+    print("Your goal is to complete each task so that you can reach your village on the other side of the woods.")
+    print("Are you ready for the adventure of a lifetime?")
     player_name = input("What is your name? ")
-    print(f"Welcome {player_name} to the Damned Willow Forest! You find yourself standing at the dense growing trees, wondering how you will get by. As you look ahead, you notice a signpost that lists several tasks that must be completed before you can continue on your journey. The tasks you must complete will test your skills and determination so you can get home. Are you ready? Take a deep breath, and let the adventure begin!")
-    choice = input("Are you ready to continue? Type 'yes' or 'no': ")
-    if choice == 'yes':
+    print(f"Welcome {player_name} to the Damned Willow Forest!")
+    print("You find yourself standing at the dense growing trees, wondering how you will get by.")
+    print("As you look ahead, you notice a signpost that lists several tasks that must be completed")
+    print("before you can continue on your journey.")
+    print("The tasks you must complete will test your skills and determination so you can get home.")
+    print("Are you ready? Take a deep breath, and let the adventure begin!")
+    choice = input("Are you ready to continue? Type 'y' for yes or 'n' for no: ")
+    if choice == 'y':
         print(f"Here is some valid information about your character: \n Health: {player_health}\n Attack: {player_attack}\n Defense: {player_defense}\n Inventory: {player_inventory}\n")
         input("Press any key to enter the woods: ")
         path_choice()
-    else:
+    elif choice == 'n':
         print("Thanks for playing! Have a good day!")
+        game_over()
+    else:
+        print("Invalid choice, please type 'y' for yes or 'n' for no")
+        start_game()
         return
 
 # Display the different path choices
 def path_choice():
-    print("You find yourself at a fork in the road. Do you want to go left or right?")
+    print("Lets get started. Do you want to move forward? Type 'y' for yes and 'n' for no and quit game")
     choice = input("> ")
-    if choice == 'left':
+    if choice == 'y':
         riddle_encounter()
-    elif choice == 'right':
-        player_encounter_goblin()
+    elif choice == 'n':
+        game_over()
     else:
-        print("Invalid choice, please type 'left' or 'right'")
+        print("Invalid choice, please type 'y' or 'n'")
         path_choice()
 
 # Solve the riddle, gives different options for the player to choose from
@@ -49,7 +61,7 @@ def riddle_encounter():
         path_choice_2()
     else:
         print("Incorrect. You have failed the task.")
-        player_death()
+        game_over()
 
 # Fight the enemy
 def player_encounter_goblin():
@@ -66,7 +78,7 @@ def player_encounter_goblin():
             goblin_health -= player_attack
             if goblin_health <= 0:
                 print("You have defeated the goblin! You may continue on your journey.")
-                path_choice_4()
+                meet_wizard()
                 return
             else:
                 print(f"The goblin now has {goblin_health} health.")
@@ -78,40 +90,32 @@ def player_encounter_goblin():
                     choice = input("Do you want to attack or run? ")
         elif choice == 'run':
             print("You run away from the goblin.")
-            path_choice_4()
+            meet_wizard()
             return
         else:
             print("Invalid choice, please type 'attack' or 'run'")
             choice = input("> ")
 
 def path_choice_2():
-    print("You find yourself at another fork in the road. Do you want to go right or forward?")
+    print("You get further in to the forest, there are huge tree logs laying everywhere.")
+    print("There is one big one in the middle of the road, you have to climb over it to keep going")
+    print("Type 'y' for yes to climb the log or type 'n' for no and surrender the game.")
     choice = input("> ")
-    if choice == 'right':
-        player_encounter_goblin()
-    elif choice == 'forward':
-        item_encounter()
+    if choice == 'y':
+        path_choice_3()
+    elif choice == 'n':
+        game_over()
     else:
-        print("Invalid choice, please type 'right' or 'forward'")
-        path_choice()
-
-def item_encounter():
-    items = ["A sword of fire", "A shield of ice","A ring of invisibility"]
-    item = random.choice(items)
-    print("You come across a chest with an item inside!")
-    print(f"You found: {item}")
-    global player_inventory
-    player_inventory.append(item)
-    print(f"you now have {player_inventory} in your inventory")
-    path_choice_3()
+        print("Invalid choice, please type 'y' or 'n'")
+        path_choice_2()
 
 def path_choice_3():
     print("You can barely see anything now, the light that once shined through the tree tops is now a distant memory")
     print("You sense that you are not alone, someone is watching you. But where?")
     print("You stop in your tracks, the smell of something rotten hits you!")
-    print("Do you want to keep going? yes or no?")
+    print("Do you want to keep going? Type 'y' for yes or 'n' for no and move along.")
     choice = input("> ")
-    if choice == 'yes':
+    if choice == 'y':
         player_encounter_goblin()
     else:
         print("You have no choice")
@@ -157,8 +161,10 @@ def wild_stream():
 
     if choice == "1":
         print("You swim over the wild stream and make it safely to the other side.")
+        complete_game()
     elif choice == "2":
         print("You jump over the wild stream and make it safely to the other side.")
+        complete_game()
     else:
         print("Invalid choice. Please enter 1 or 2.")
         wild_stream()
@@ -169,14 +175,15 @@ def complete_game():
     print("'You made it!!!' 'You're alive!!!' the gnomes chant.")
     print("Well done, you made it to the other side. Go home and rest, you deserve it!")
     again = input("Do you want to play again? (yes or no)")
-    if again == "yes":
+    choice = input("> ")
+    if choice == "yes":
         start_game()
-    elif again == "no":
+    elif choice == "no":
         print("Thank you for playing!")
     else:
         print("Invalid choice. Please enter yes or no.")
-        teleport()
-    
+        complete_game()
+
 # Explains options the player has if they die
 def player_death():
     global player_health
@@ -190,5 +197,15 @@ def player_death():
         print("You have chosen not to use your healing potion. Game over.")
         return
 
+def game_over():
+    print("This is where your story end. Do you want to try again?")
+    print("Type 'y' for yes and start over or type 'n' for no and quit game")
+    choice = input(">")
+    if choice == 'y':
+        start_game()
+    elif choice == 'n':
+        print("Thank you for playing and welcome back!")
+    else:
+        print("Invalid choice. Please enter y or n.")
 # Start the game
 start_game()
