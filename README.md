f505781e5d08e68dad2f1086c2265ae868ef5339
Molly Adamsson <adamssonmolly@gmail.com>
2023-01-28 10:05:02 +0000

    Add content

--- README.md
+++ README.md
@@ -1,31 +1,95 @@
-![CI logo](https://codeinstitute.s3.amazonaws.com/fullstack/ci_logo_small.png)
+# The adventure of the Damned Willow Forest!
 
-Welcome Molly Adamsson,
+* This is a text adventure game based off of my own imagination. Worth mentioning is the fact that it has some inspiration taken from The lord of the rings. The player is presented with the context of them standing outside of a forest, and they have to go through it if they want to go back home. The sign at the edge of the forest explains that they will encounter som obstacles and mystical creatures on their way. Well inside the player will meet everythig between a goblin to a witch, each of these characters will offer something valuable. At the end the player will meet a Wizard, he wants to know if the player has seen his treasure, and if so the Wizard will help the player to get to the end of the forest so that he can go home, if not the player has to get over the wild stream by themself.
 
-This is the Code Institute student template for deploying your third portfolio project, the Python command-line project. The last update to this file was: **August 17, 2021**
+* The player also has an inventory where they can carry the treasures and valuables.
 
-## Reminders
+## Flowchart
 
-* Your code must be placed in the `run.py` file
-* Your dependencies must be placed in the `requirements.txt` file
-* Do not edit any of the other files or your code may not deploy properly
+* I have created a flowchart to help myself plan out the bascis of the game. This helped me a lot when I started to plan out which path was connected to which and so on. When looking at the chart one can understand the basic knowledge of what to do.
 
-## Creating the Heroku app
+* The original one I made was not the one that resulted in the finished product, I realised that some of the steps that I first planned out was a bit unnecessary.
 
-When you create the app, you will need to add two buildpacks from the _Settings_ tab. The ordering is as follows:
+* This is the one I ended up using:
+![Flowchart]()
 
-1. `heroku/python`
-2. `heroku/nodejs`
+## User Experience
 
-You must then create a _Config Var_ called `PORT`. Set this to `8000`
+- #### First time user goals
+* As I first time user I want to know what the game is about
+* As a first time user I want to understand the basic knowledge of the game
+* As a first time user I want to understand how to continue the game
+* As a first time user I want to be able to understand the instructions
+* As a first time user I want to be able to explore all parts of the game
+* As a first time user I want to have fun!
 
-If you have credentials, such as in the Love Sandwiches project, you must create another _Config Var_ called `CREDS` and paste the JSON into the value field.
+- #### Returning user goals
 
-Connect your GitHub repository and deploy as normal.
+* As a returning visitor I want to be able to beat the game faster than before.
+* As a returning visitor I want to try to take another path and see where that leads me.
 
-## Constraints
+## Features
+* Should I add a header?
 
-The deployment terminal is set to 80 columns by 24 rows. That means that each line of text needs to be 80 characters or less otherwise it will be wrapped onto a second line.
+## The Game itself
 
------
-Happy coding!
+### Main menu
+
+* At the begining the user is asked if they want to start the game with a question asking: "Do you want to begin?" "Enter 1. to start game and enter 2. to quit".
+
+![Main menu]()
+
+### Path ways
+
+* Different pathways will be presented during the game. Each one will take the player deeper inside the woods and each one will give the player the oppertunity to choose where to go.
+
+(exempelbild här?)
+
+### Solve riddle
+
+* The first obstacle is a riddle, the player has to solve it before they can continue. If they get the riddle wrong they loose the game.
+
+### Meeting the characters
+
+* The player will get the chance of meeting a goblin, the player has to fight it and if they win they get a badge of honor collected to their inventory. 
+* The player will get the chance of meeting a witch, she will give the player a few words of wisdom and then proceed to give them a "Book of wisdom" and the player collects it as well. 
+* The player will get the chance of meeting a wizard, he will ask if the player has seen his treasure. If so and the player gives i to him he will help them to the end of the forest. If not - he says something snarky and disappears.
+
+### The wild stream
+
+* If the player doesn't help the wizard they are left alone to deal with the wild stream.
+* It is not that hard to get passed it and the player will soon after that see the end of the forest.
+
+### The end of the forest
+
+* When they arrive at the end they are greeted by a couple of gnomes.
+* The gnomes chant and exclaims happieness that the player made it out alive.
+* The player will after that be informed with that the game is over and they have the oppertunity to play again if they want.
+
+### Error messages
+
+* If the player enters an invalid answer they will be presented with an error message.
+* They can look a bitt different but here are a few of them:
+
+image
+image
+image
+
+### Player Death
+
+* There is a slight chance of the player getting killed by the Goblin, if so they will get the chance to drink the healing potion they recieved at the begining of the game. After that they will be resurrected and can continue the game.
+
+## Testing
+
+## Deployment
+
+### Heroku Deployment
+
+## Credits
+
+### content
+
+## Acknowledgements
+
+
+Molly Adamsson, 2023
--- run.py
+++ run.py
@@ -68,8 +68,9 @@ def riddle_encounter():
     answer = input("What is your answer? ")
     if answer.lower() == "2":
         clearConsole()
-        print("Correct! The chest is open! You collect the treasure inside! You may continue on your journey.")
-        print("And you continue to move further into the woods")
+        print("Correct! The chest is open! You collect the treasure inside!")
+        print("The treasure is shiny and heavy. It must be worth alot.")
+        print("You put in your backpack and start walking again.")
         clearConsole()
         path_choice_2()
     else:
@@ -80,12 +81,10 @@ def riddle_encounter():
 def player_encounter_goblin():
     goblin_health = 40
     goblin_attack = 15
-    global player_inventory 
-    player_inventory = ["Badge of Honor"]
-    print("You have come across a nasty forest goblin!")
-    print("Do you want to attack or run away?")
-    print("Enter 'a' to attack or 'r' to run away!")
-    choice = input("> ")
+    print("\033[31mYou have come across a nasty forest goblin!\033[0m")
+    print("\033[31mDo you want to attack or run away?\033[0m")
+    print("\033[31mEnter 'a' to attack or 'r' to run away!\033[0m")
+    choice = input("\033[31m>\033[0m ")
     while goblin_health > 0:
         if choice == 'a':
             global player_health
@@ -93,73 +92,55 @@ def player_encounter_goblin():
             goblin_health -= player_attack
             if goblin_health <= 0:
                 clearConsole()
-                print("You have defeated the goblin! You may continue on your journey.")
-                player_inventory.append("Badge of Honor")
-                print(f"You have recieved a 'Badge of Honor', it has been added to your inventory")
-                print("Well done, where do you want to go now?")
-                print("Enter 'r' to go right or enter 'f' to go forward!")
-            choice = input("> ")
-            if choice == 'r':
-                print(player_inventory)
-                if "Book of Wisdom" in player_inventory and "Badge of Honor" in player_inventory:
+                print("\033[32mYou have defeated the goblin! You may continue on your journey.\033[0m")
+                print("\033[32mYou have received a 'Badge of Honor', only brave ones get this.\033[0m")
+                print("\033[32mDo you want to continue your journey? Enter '1' for yes and '2' for no and end game.\033[0m")
+                choice = input("\033[32m>\033[0m ")
+                if choice == '1':
                     meet_wizard()
+                elif choice == '2':
+                    game_over()
                 else:
-                    path_choice_4()
-            elif choice == 'f':
-                if "Book of Wisdom" in player_inventory and "Badge of Honor" in player_inventory:
-                    meet_wizard()
-                else:
-                    print("You are not ready to go there yet, try another way.")
-                    choice = input("Enter '1' to go to the right")
-                    if choice == '1':
-                        path_choice_4()
-                    else:
-                        print("Invalid choice, please type 'r' or 'f'")
-                    return
+                    print("\033[31mInvalid choice, please enter '1' or '2'\033[0m")
+                    choice = input("\033[31m>\033[0m ")
             else:
-                print(f"The goblin now has {goblin_health} health.")
+                print("\033[31mThe goblin now has " + str(goblin_health) + " health.\033[0m")
                 player_health -= goblin_attack
                 if player_health <= 0:
                     clearConsole()
                     player_death()
                 else:
-                    print(f"You now have {player_health} health.")
-                    print("Do you want to attack or run? enter 1 to attack and 2 to run")
-                    choice = input(">")
-        elif choice == '2':
+                    print("\033[31mYou now have " + str(player_health) + " health.\033[0m")
+                    print("\033[31mDo you want to attack or run? Enter 'a' to attack and 'r' to run.\033[0m")
+                    choice = input("\033[31m>\033[0m ")
+        elif choice == 'r':
             run_away_chance = random.randint(10,14)
             if run_away_chance <= 30:
-                print("You managed to run away safely.")
-                player_inventory.append("Badge of Honor")
-                print(f"You have received a 'Badge of Honor' for at least trying! It has been added to your inventory.")
                 clearConsole()
-                print("Enter 'a' to go right or enter 'b' to go forward!")
-            choice = input("> ")
-            if choice == 'a':
-                path_choice_4()
-            elif choice == 'f':
-                if "Book of Wisdom" in player_inventory and "Badge of Honor" in player_inventory:
+                print("\033[32mYou managed to run away safely from the goblin.\033[0m")
+                print("\033[32mDo you want to continue your journey? Enter '1' for yes and '2' for no and end game.\033[0m")
+                choice = input("\033[32m>\033[0m ")
+                if choice == '1':
                     meet_wizard()
+                elif choice == '2':
+                    game_over()
                 else:
-                    print("You are not ready to go there yet, try another way.")
-                    choice = input("Enter 'x' to go to the right")
-                    if choice == 'x':
-                        path_choice_4()
-                    else:
-                         print("Invalid choice, please type 'a' or 'b'")
-                return
+                    print("\033[31mInvalid choice, please enter '1' or '2'\033[0m")
+                    choice = input("\033[31m>\033[0m ")
             else:
-                print("You failed to run away and the goblin hit you.")
+                clearConsole()
+                print("\033[31mYou failed to run away and the goblin hit you.\033[0m")
                 player_health -= goblin_attack
                 if player_health <= 0:
-                    clearConsole()
                     player_death()
                 else:
-                    print(f"You now have {player_health} health.")
-                    choice = input("Do you want to attack or run? ")
+                    print("\033[31mYou now have {player_health} health.\033[0m")
+                    print("\033[31mDo you want to attack or run? Enter 'a' to attack and 'r' to run.\033[0m")
+                    choice = input("\033[31m>\033[0m ")
         else:
-            print("Invalid choice, please type 'a' or 'r'")
-            choice = input("> ")
+            print("\033[31mInvalid choice, please type 'a' or 'r'\033[0m")
+            choice = input("\033[31m>\033[0m ")
+
 
 # Path choice nr 2, here the player has to make choice if to go to path choice 3 or path choice 4
 def path_choice_2():
@@ -167,16 +148,16 @@ def path_choice_2():
     print("The soft rustling of leaves scared you a bit, anything could be hiding in the bushes")
     print("You get further in to the forest, there are huge tree logs laying everywhere.")
     print("There is one big one in the middle of the road, you have to climb over it to keep going")
-    print("Type 'y' for yes to climb the log or type 'g' to go another way.")
+    print("Type '1' for yes to climb the log or type '2' to go another way.")
     choice = input("> ")
-    if choice == 'y':
+    if choice == '1':
         clearConsole()
         path_choice_3()
-    elif choice == 'g':
+    elif choice == '2':
         clearConsole()
         path_choice_4()
     else:
-        print("Invalid choice, please type 'y' or 'n'")
+        print("Invalid choice, please type '1' or '2'")
         path_choice_2()
     return
 
@@ -229,54 +210,59 @@ def path_choice_4():
                 path_choice_4()
         clearConsole()
     else:
-        print("Invalid choice, please type 'left' or 'forward'")
-        path_choice_4()
+        print("Invalid choice, please type 'left' or 'f'")
         return
 
+def path_choice_5():
+    print("You move along, wondering what the beautiful witch meant")
+    print("How many times will you rise and fall?")
+    print("You study the the binders of the book when all of a sudden")
+    print("three small forest gnomes appear, right in fron of you")
+    print("You: \"Hello, can I help you\" ")
+    print("The gnomes look at each other, and then back at you")
+    print("Then all of them open their mouths an at the same time they say: ")
+    print(" \"You have to hurry, he is waiting for you\"")
+    print("You:\"Huh, who is?\" ")
+    print("\"You will find out soon, just hurry!\" They say.")
+    print("They then grab each others hands and scurries off")
+    print("You stand still for quick moment, wondering what just happend")
+    print("You then say to yourself: ")
+    print("\"Well I cant stay here, can I? I have to keep going\"")
+    print("Do you want to continue? typ 'y' for yes and 'n' for no and end game")
+    choice = input("> ")
+    if choice == 'y':
+        meet_wizard()
+    elif choice == 'n':
+        game_over()
+    else:    
+        print("Invalid choice, please type 'y' or 'n'")
+        return
+
+    
 # Function that introduce the witch, here the player is given an item that must be collected to 
 # keep going. The player must have the "Book of wisdom" and "Badge of honor" to proceed to the Wizard.
 def meet_witch():
-    global player_inventory
     print("A beautiful woman appears")
-    print("Ah the trees and the gnomes were right, the witch says with a soft smile")
+    print("\Ah the wispers were right\", the witch says with a soft smile")
     print("There is a human here, I've been looking all over for you")
     print("I need to tell you something, a few words of wisdom to keep in mind")
     print("You ready? Okay, listen closely")
     print("The greatest glory in living lies not in never falling, but in rising every time we fall")
     print("You need to remember this for the rest of your journey, okay?")
     print("Matter of fact, take this book, it is filled with knowledge")
-    player_inventory.append("Book of Wisdom")
     print("You have collected the book of wisdom. It has been added to your inventory")
     print("Suddenly the beautiful witch disapears and you're all alone")
-    print("Where do you want to go now? Left or forward?")
-    print("Enter 'l' to go left or enter 'f' to go forward!")
+    print("Do you want to continue your journey? Enter 'y' for yes or 'n' for no.")
     choice = input("> ")
-    if choice == 'l':
-        if "Book of Wisdom" in player_inventory and "Badge of Honor" in player_inventory:
-            print("Oh no, you've reached a dead end!")
-            print("Enter 'f' to go forward")
-            choice = input(">")
-            if choice == 'f':
-                meet_wizard()
-            else:
-                print("Invalid choice, try again")
-                clearConsole()
-                return    
-        else:
-            path_choice_3()
-    elif choice == 'f':
-        if "Book of Wisdom" in player_inventory and "Badge of Honor" in player_inventory:
-            clearConsole()
-            meet_wizard()
-        else:
-            print("You are not ready to go there yet, try another way.")
-            choice = input("Enter 'l' to go to the left")
-            if choice == 'l':
-                clearConsole()
-                path_choice_3()
-            else:
-                print("Invalid choice, please type 'l' or 'f'")
-            return
+    if choice == 'y':
+        clearConsole()
+        path_choice_5()
+    elif choice == 'n':
+        clearConsole()
+        game_over()
+    else:
+        print("Invalid choice, please type 'y' or 'n'")
+        meet_witch()
 
 # Function that introduces the Wizard, if the player give away the treasure that was collected during
 # the riddle encounter the Wizard will help the player to reach the end of the forest, if
